7d11afc9287954c76e47ba35c412983029b1735a
Het-Patel69 <phet9429@gmail.com>
2023-09-21 15:49:45 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import SelectCategoryPage from "./pages/SelectCategoryPage";
+import QuizPage from "./pages/QuizPage";
+import HomePage from "./pages/HomePage";
+import "./App.css";
+import { createContext, useState } from "react";
+import { ConfigContext } from "./context/ConfigContext";
+import ScoreContext from "./context/ScoreContext";
 
 function App() {
+  const [config, setConfig] = useState({
+    category: false,
+    number: 10,
+    difficulty: false,
+    categoryName: "Any Category"
+  });
+  const [categories, setCategories] = useState(null);
+  const [score, setScore] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ConfigContext.Provider
+        value={{ config, setConfig, categories, setCategories }}
+      >
+        <ScoreContext.Provider value={{score, setScore}}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" Component={HomePage} />
+              <Route path="/quiz" Component={QuizPage} />
+              {/* <Route path="/score" Component={ScorePage} /> */}
+            </Routes>
+          </BrowserRouter>
+        </ScoreContext.Provider>
+      </ConfigContext.Provider>
     </div>
   );
 }
